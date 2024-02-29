a10cf5af795ec6d661fa5925977c3e8563283c44
isaacmackley <isaac.mackley@hotmail.co.uk>
2024-02-29 16:19:00 +0000

    pdos_plot cli update

--- cli/pdos.py
+++ cli/pdos.py
@@ -4,9 +4,9 @@ from cp2k_utils.pdos import pdos_plot
 
 def main():
   parser = argparse.ArgumentParser(description='Automatic plotting of DOS')
-  parser.add_argument('elements', help='list of elements (in correct order)')
-  parser.add_argument('spin', help='whether calculation is spin polarised')
-  parser.add_argument('sigma', type=float, help='sigma for smearing')
+  parser.add_argument('elements', nargs='+', help='list of elements (in correct order)')
+  parser.add_argument('-p', '--spin', type=bool, help='whether calculation is spin polarised')
+  parser.add_argument('-s', '--sigma', type=float, help='sigma for smearing')
     
   args = parser.parse_args()
     
