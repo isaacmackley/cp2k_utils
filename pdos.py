f5d29548a820f67d9fcd1e04e86ff9c491eb49c2
isaacmackley <63374221+isaacmackley@users.noreply.github.com>
2025-03-19 18:36:08 +0000

    added ipr plotting

--- pdos.py
+++ pdos.py
@@ -365,3 +365,204 @@ def pdos_plot(elements,spin=True,sigma=0.003,vbm=0,grid=True,xmin=-3,xmax=6,figw
     plt.savefig('Total Density of States', bbox_inches='tight')
 
     plt.show()
+
+
+
+def ipr_pdos_plot(elements,spin=True,sigma=0.003,vbm=0,grid=True,xmin=-3,xmax=6,figw=25,figh=10):
+    """
+    
+    """
+
+        
+    k=0
+    names={}
+    plots={}
+
+    if spin==True:
+        for element in elements:
+            k=k+1
+            for file in os.listdir('.'):
+                if fnmatch.fnmatch(file, '*ALPHA_k'+str(k)+'*'):
+                    alpha=file
+            for file in os.listdir('.'):
+                if fnmatch.fnmatch(file, '*BETA_k'+str(k)+'*'):
+                    beta=file
+
+            element_no=f"element{k}"
+            
+            names[element_no] = smeared_pdos([alpha,beta],sigma)
+    elif spin==False:
+        for element in elements:
+            k=k+1
+            for file in os.listdir('.'):
+                if fnmatch.fnmatch(file, '*-k'+str(k)+'*'+'.pdos'):
+                    alpha=file
+                elif fnmatch.fnmatch(file, '*k'+str(k)+'*'+'.pdos'):
+                    alpha=file
+
+            element_no=f"element{k}"
+            
+            names[element_no] = smeared_pdos([alpha],sigma)
+
+
+    n=1
+    while n <= len(elements):
+    
+        x = []
+        y = []
+
+        s = []
+        p = [] 
+        d = [] 
+        f = []
+
+        y_2 = []
+
+        s_2 = []
+        p_2 = [] 
+        d_2 = [] 
+        f_2 = []
+
+        i=1
+
+        if spin==True:
+            orbs = (len(names[f'element{n}'][0])-1)/2
+        elif spin==False:
+            orbs = (len(names[f'element{n}'][0])-1)
+
+        while i < len(names[f'element{n}']):
+
+            x.append(names[f'element{n}'][i][0])
+            
+            s.append(names[f'element{n}'][i][1])
+
+            if orbs == 1:
+                y = [s]
+                if spin==True:
+                    s_2.append(names[f'element{n}'][i][2]*-1)
+                    y_2 = [s_2]
+
+            if orbs == 2:
+                p.append(names[f'element{n}'][i][2])
+                y = [s, p]
+                if spin==True:
+                    s_2.append(names[f'element{n}'][i][3]*-1)
+                    p_2.append(names[f'element{n}'][i][4]*-1)
+                    y_2 = [s_2, p_2]
+
+            if orbs == 3:
+                p.append(names[f'element{n}'][i][2])
+                d.append(names[f'element{n}'][i][3])
+                y = [s, p, d]
+                if spin==True:
+                    s_2.append(names[f'element{n}'][i][4]*-1)
+                    p_2.append(names[f'element{n}'][i][5]*-1)
+                    d_2.append(names[f'element{n}'][i][6]*-1)
+                    y_2 = [s_2, p_2, d_2]
+
+            if orbs == 4:
+                p.append(names[f'element{n}'][i][2])
+                d.append(names[f'element{n}'][i][3])
+                f.append(names[f'element{n}'][i][4])
+                y = [s, p, d, f]
+                if spin==True:
+                    s_2.append(names[f'element{n}'][i][5]*-1)
+                    p_2.append(names[f'element{n}'][i][6]*-1)
+                    d_2.append(names[f'element{n}'][i][7]*-1)
+                    f_2.append(names[f'element{n}'][i][8]*-1)
+                    y_2 = [s_2, p_2, d_2, f_2]
+                        
+
+            i=i+1
+
+        element_name = f'{elements[n-1]}'
+        plots[f'{element_name}_x'] = x
+        plots[f'{element_name}_alpha_tot'] = sum(map(np.array, y))
+        if spin==True:
+            plots[f'{element_name}_beta_tot']  = sum(map(np.array, y_2))
+
+    
+    plt.figure(figsize=(figw,figh))
+    plt.title("Total Density of States", size=40)
+    
+    ax = plt.subplot()
+    ax.set_xlabel('Energy (eV)', size=30) 
+    ax.set_ylabel('Denisty of States (arb.)', size=30)
+
+    if spin==True:
+        ymax = max(max([y for x, y in zip(plots[f'{elements[0]}_x'], plots[f'{elements[0]}_alpha_tot']) if xmin <= x <= xmax]), max([y for x, y in zip(plots[f'{elements[0]}_x'], plots[f'{elements[0]}_beta_tot']) if xmin <= x <= xmax], key=abs))
+    elif spin==False:
+        ymax = max([y for x, y in zip(plots[f'{elements[0]}_x'], plots[f'{elements[0]}_alpha_tot']) if xmin <= x <= xmax])
+    #plt.axvline(x=vbm, color='k', linestyle='--', alpha=0.5)
+
+    #plt.axvline(x=0, color='k', linestyle='--', alpha=0.5)
+    ax.axhline(y=0, color='k', linestyle='-', alpha=0.5)
+
+    j=1
+    while j <= len(elements):
+        if spin==True:
+            ymax = max(ymax,max([y for x, y in zip(plots[f'{elements[j-1]}_x'], plots[f'{elements[j-1]}_alpha_tot']) if xmin <= x <= xmax]), max([y for x, y in zip(plots[f'{elements[j-1]}_x'], plots[f'{elements[j-1]}_beta_tot']) if xmin <= x <= xmax], key=abs))
+        if spin==False:
+            ymax = max(ymax,max([y for x, y in zip(plots[f'{elements[j-1]}_x'], plots[f'{elements[j-1]}_alpha_tot']) if xmin <= x <= xmax]), key=abs)
+        ax.plot(plots[f'{elements[j-1]}_x'], plots[f'{elements[j-1]}_alpha_tot'], marker = '',lw=2,c=ELEMENT_COLOURS[elements[j-1]], label=f'{elements[j-1]}')
+        if spin==True:
+            ax.plot(plots[f'{elements[j-1]}_x'], plots[f'{elements[j-1]}_beta_tot'],lw=2,c=ELEMENT_COLOURS[elements[j-1]], marker = '')
+        j=j+1
+
+    if grid==True:
+        plt.grid(visible=1)
+    else:
+        plt.grid(visible=0)
+
+    ymax = np.ceil(ymax*10)/10
+    
+    if ymax > 1.0:
+        plt.yticks(np.linspace(-ymax,ymax,int(((ymax*5) + 1)/2)*2 + 1),labels=[])
+    if 0.5 < ymax <= 1.0:
+        plt.yticks(np.linspace(-ymax,ymax,int(((ymax*20) + 1)/2)*2 + 1),labels=[])
+    if 0.2 < ymax <= 0.5:
+        plt.yticks(np.linspace(-ymax,ymax,int(((ymax*40) + 1)/2)*2 + 1),labels=[])
+    if ymax <= 0.2:
+        plt.yticks(np.linspace(-ymax,ymax,int(((ymax*80) + 1)/2)*2 + 1),labels=[])
+        
+    if grid==True:
+            plt.xticks(np.linspace(xmin,xmax,int((abs(xmin)+xmax)*4)+1),fontsize=20)
+    else:
+            plt.xticks(np.linspace(xmin,xmax,int((abs(xmin)+xmax))+1),fontsize=20)
+
+    plt.xlim([xmin,xmax])
+    if spin==True:
+        plt.ylim([-ymax,ymax])
+    if spin==False:
+        plt.ylim([0,ymax])
+
+    ax2 = ax.twinx()
+    ax2.set_ylabel('IPR', size=30)
+
+    ipr_alpha_x = []
+    ipr_alpha_y = []
+    ipr_beta_x = []
+    ipr_beta_y = []
+
+    with open('./IPR_output-Alpha.dat', 'r') as file:
+        for line in file:
+            columns = line.split()
+            ipr_x_alpha.append(float(columns[0])*Ha_to_eV-vbm)
+            ipr_y_alpha.append(float(columns[1]))
+    with open('./IPR_output-Beta.dat', 'r') as file:
+        for line in file:
+            columns = line.split()
+            ipr_x_beta.append(float(columns[0])*Ha_to_eV-vbm)
+            ipr_y_beta.append(float(columns[1]))
+   
+    for xi, yi in zip(ipr_x_alpha, ipr_y_alpha):
+        ax2.vlines(xi, 0, yi, colors='blue', linewidth=1)
+    for xi, yi in zip(ipr_x_beta, ipr_y_beta):
+        ax2.vlines(xi, 0, yi, colors='orange', linewidth=1)
+
+    
+    plt.legend(markerscale=10.0, fontsize=20)
+
+    plt.savefig('Total Density of States', bbox_inches='tight')
+
+    plt.show()
